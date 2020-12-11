e78437811b94b9f9ab44c0125e88a67d99583cd1
Sarveshlal <68782507+Sarveshlal@users.noreply.github.com>
2020-12-11 10:47:30 +0530

    Add files via upload

--- weather.js
+++ weather.js
@@ -7,15 +7,22 @@ request.send();
 request.onload = function() {
 	
 	var data = JSON.parse(this.response);
-    var srequest = new XMLHttpRequest();
-    lat = data[104].latlng[0];
-	lon = data[104].latlng[1];
-	srequest.open('GET',`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f65b074e7e5f9345ad4649fe375b9294`,true);
-	srequest.send();
-	srequest.onload = function() {
-		var weatherdata = JSON.parse(this.response);
-		console.log(weatherdata);
-		console.log(weatherdata.main.temp)
-		console.log(weatherdata.weather[0].description);
+	var lat = data[104].latlng[0];
+	var lon = data[104].latlng[1];
+	var cityname = data[159].name;
+	function weatherapi(url){
+		var srequest = new XMLHttpRequest();
+		srequest.open('GET',url,true);
+		srequest.send();
+		srequest.onload = function() {
+			var weatherdata = JSON.parse(this.response);
+			console.log("Country: ",weatherdata.name);
+			console.log("Temperature: ",weatherdata.main.temp)
+			console.log("Weather Description:",weatherdata.weather[0].description);
+		}
 	}
+	var url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f65b074e7e5f9345ad4649fe375b9294` 
+	weatherapi(url)
+	var url = `http://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=f65b074e7e5f9345ad4649fe375b9294`
+	weatherapi(url);
 }
